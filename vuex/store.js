0c2c442940a572100532ca51853d5d39e8ed78f6
andrewgurung <andrewgurungg@gmail.com>
2017-05-09 18:57:52 -0400

    Add mutators

--- vuex/store.js
+++ vuex/store.js
@@ -11,6 +11,32 @@ const state = {
 
 // define the possible mutations that can be applied to our state
 const mutations = {
+    ADD_NOTE (state) {
+        const newNote = {
+            text: 'New note',
+            favorite: false
+        }
+        // only mutators can mutate the state
+        state.notes.push(newNote)
+        state.activeNote(newNote)
+    },
+
+    EDIT_NOTE (state, text) {
+        state.activeNote.text = text
+    },
+
+    DELETE_NOTE (state) {
+        state.notes.$remove(state.activeNote)
+        state.activeNote = state.notes[0]
+    },
+
+    TOGGLE_FAVORITE (state) {
+        state.activeNote.favorite = !state.activeNote.favorite
+    },
+
+    SET_ACTIVE_NOTE (state, note) {
+        state.activeNote = note
+    }
 
 }
 
